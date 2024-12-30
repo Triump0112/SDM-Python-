8398783b2ffd3af8aba95dd68cbb6d0e87adc156
Triump0112 <127780189+Triump0112@users.noreply.github.com>
2024-12-30 22:26:22 +0530

    Update pseudo_absence_generator.py

--- Modules/pseudo_absence_generator.py
+++ Modules/pseudo_absence_generator.py
@@ -1,12 +1,11 @@
-import numpy as np 
-import pandas as pd 
-from shapely.geometry import Point, shape, Polygon
+import numpy as np
+import pandas as pd
+from shapely.geometry import Point, shape
 from tqdm.notebook import tqdm
 import concurrent.futures
+from scipy.spatial.distance import cdist
 from . import features_extractor
 from . import LULC_filter
-from scipy.spatial.distance import cdist
-
 
 feature_cols = [
     'annual_mean_temperature',
@@ -34,125 +33,86 @@ feature_cols = [
     'topsoil_texture',
     'subsoil_texture',
     'elevation'
-     ]
+]
 
 class PseudoAbsences:
-    def __init__(self,ee):
-        self.ee = ee 
+    def __init__(self, ee):
+        self.ee = ee
         self.feature_extractor = features_extractor.Feature_Extractor(self.ee)
         self.modeLULC = LULC_filter.LULC_Filter(self.ee).load_modeLULC()
         ecoregions = ee.FeatureCollection('RESOLVE/ECOREGIONS/2017')
-        india = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017') \
-            .filter(ee.Filter.eq('country_co', 'IN'))
-        
-        ecoregion_geom = malabar_ecoregion = ecoregions \
-            .filterBounds(india) \
-            .filter(ee.Filter.eq('ECO_NAME', 'Malabar Coast moist forests')) \
-            .first()
+        india = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(ee.Filter.eq('country_co', 'IN'))
+        ecoregion_geom = ecoregions.filterBounds(india).filter(ee.Filter.eq('ECO_NAME', 'Malabar Coast moist forests')).first()
         self.ecoregion_geom = ecoregion_geom.geometry()
 
-
-    def reliability(self,presence_points_df, absence_point_dict):
+    def reliability(self, presence_points_df, absence_point_dict):
         presence_features = presence_points_df.iloc[:, 2:]
-
         absence_point_features = [float(absence_point_dict[i]) for i in absence_point_dict if absence_point_dict[i] is not None]
         distances = cdist([absence_point_features], presence_features, metric='euclidean')
-
         similarities = np.exp(-distances**2 / (2 * presence_features.shape[1]))
-        # print(*similarities)
         mean_similarity = np.nanmean(similarities)
         reliability = 1 - mean_similarity
         return reliability
 
-
-
-    def generate_batch_points(self,minx, miny, maxx, maxy, batch_size=1000):
-
+    def generate_batch_points(self, minx, miny, maxx, maxy, batch_size=1000):
         rand_lons = np.random.uniform(minx, maxx, batch_size)
         rand_lats = np.random.uniform(miny, maxy, batch_size)
         return list(zip(rand_lons, rand_lats))
 
-    def generate_pseudo_absences(self,presence_df):
-        modelLULC = self.modeLULC
-        num_points = len(presence_df)  # Get number of points from presence_df
-        # num_points=10
-        ee = self.ee 
+    def generate_pseudo_absences(self, presence_df):
+        num_points = len(presence_df)
         ecoregion_geom = self.ecoregion_geom
-
-        # Load the India boundaries dataset
-        
-        
-
-        print(f"Target number of points to generate: {num_points}")
-
-        # Create a global DataFrame to store the generated points
-        global_df = pd.DataFrame(columns=['longitude', 'latitude', 'reliability', 'normalized_features'])
-
         eco_region_polygon = shape(ecoregion_geom.getInfo())
         bounds = eco_region_polygon.bounds
         minx, miny, maxx, maxy = bounds
-
+        global_df = pd.DataFrame(columns=['longitude', 'latitude', 'reliability', 'normalized_features'])
         total_attempts = 0
-        batch_size = 1000  # Process points in batches
+        batch_size = 1000
 
         with tqdm(total=num_points, desc="Generating points") as pbar:
             while len(global_df) < num_points:
-                # Generate a batch of random points
                 batch_points = self.generate_batch_points(minx, miny, maxx, maxy, batch_size)
                 total_attempts += batch_size
 
-                # Process batch in parallel
                 with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
-                    futures = []
-                    for rand_lon, rand_lat in batch_points:
-                        futures.append(executor.submit(self.process_single_point,
-                                                    rand_lon,
-                                                    rand_lat,
-                                                    eco_region_polygon,
-                                                    presence_df,
-                                                    ))
+                    future_to_point = {
+                        executor.submit(self.process_single_point, lon, lat, eco_region_polygon, presence_df): (lon, lat)
+                        for lon, lat in batch_points
+                    }
 
-                    for future in concurrent.futures.as_completed(futures):
+                    for future in concurrent.futures.as_completed(future_to_point):
                         result = future.result()
                         if result is not None:
                             global_df = pd.concat([global_df, result], ignore_index=True)
                             pbar.update(1)
 
-                            if len(global_df) >= num_points:
-                                break
+                        if len(global_df) >= num_points:
+                            break
 
         print(f"\nFinal Statistics:")
         print(f"Total points generated: {len(global_df)}")
         print(f"Total attempts made: {total_attempts}")
-        print(f"Overall success rate: {(len(global_df)/total_attempts)*100:.2f}%")
+        print(f"Overall success rate: {(len(global_df) / total_attempts) * 100:.2f}%")
 
-        # Extract normalized features into separate columns
-        
         for i, col in enumerate(feature_cols):
             global_df[col] = global_df['normalized_features'].apply(lambda x: x[col])
-
-        # Drop the normalized_features column as we now have individual columns
         global_df = global_df.drop('normalized_features', axis=1)
 
         return global_df
-    
 
-
-    def process_single_point(self,rand_lon, rand_lat, eco_region_polygon, presence_df):
-        modelLULC = self.modeLULC
-        ee = self.ee 
+    def process_single_point(self, rand_lon, rand_lat, eco_region_polygon, presence_df):
         try:
             if eco_region_polygon.contains(Point(rand_lon, rand_lat)):
-                point = ee.Geometry.Point([rand_lon, rand_lat])
-                lulc_value = modelLULC.reduceRegion(
-                    ee.Reducer.mode(),
+                point = self.ee.Geometry.Point([rand_lon, rand_lat])
+                lulc_value = self.modeLULC.reduceRegion(
+                    self.ee.Reducer.mode(),
                     point,
                     scale=10,
                     maxPixels=1e9
                 ).get('label').getInfo()
 
                 if lulc_value == 1:
-                    bioclim_values_random =  self.feature_extractor.get_feature_values_at_point(rand_lat, rand_lon)
+                    bioclim_values_random = self.feature_extractor.get_feature_values_at_point(rand_lat, rand_lon)
                     normalized_bioclim_values_random = self.feature_extractor.normalize_bioclim_values(bioclim_values_random)
                     reliability_value = self.reliability(presence_df, normalized_bioclim_values_random)
 
@@ -164,10 +124,6 @@ class PseudoAbsences:
                             'normalized_features': normalized_bioclim_values_random
                         }
                         return pd.DataFrame([row])
-        except Exception as e:
+        except Exception:
             pass
         return None
-
-
-
-# Generate pseudo-absence points
